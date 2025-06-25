03804d9151f7b1c315e3aaff5e42dda07290d2d9
devq-ai <dion@devq.ai>
2025-06-25 01:39:38 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<style lang="postcss">
-	.header {
-		height: 100%;
-		background: linear-gradient(180deg, #FF10F0 60%, transparent 40%);
-	}
-</style>
+<script lang="ts">
+	import Incidents from '$lib/components/Incidents.svelte';
+	import Status from '$lib/components/Status.svelte';
+	import System from '$lib/components/System.svelte';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
 
 <div class="w-full bg-accent header min-h-[20vh] lg:min-h-[25vh] flex items-end justify-center">
 	<div class="h-full w-full mx-2 md:mx-12">
@@ -25,6 +28,6 @@
 <style lang="postcss">
 	.header {
 		height: 100%;
-		background: linear-gradient(180deg, theme('colors.accent') 60%, transparent 40%);
+		background: linear-gradient(180deg, #FF10F0 60%, transparent 40%);
 	}
 </style>
